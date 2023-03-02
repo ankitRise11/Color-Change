fc921362eacd2be3215081ab5244093f3890c1e4
Ankit Mahotla <ankit@rise11.com>
2023-03-02 15:01:10 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            my: 4,
+            width: 200,
+            height: 170,
+            border: 1,
+            borderColor: "black",
+            backgroundColor: color
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+            }}
+          >
+            {color ? color : 'Empty List'}
+          </Typography>
+        </Box>
+        <TextField
+          label="Add Color Name"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+      </Box>
+    </Container>
   );
 }
 
